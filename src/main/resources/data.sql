850829a75c65a961e57067238a3df7372db99ff6
Alejandro <alejandro.ramos-salmeron@allianz.com>
2024-06-21 17:14:39 +0200

    First Deployment

---
+++ src/main/resources/data.sql
@@ -0,0 +1,91 @@
+-- Inserciones para la tabla SECTIONS
+INSERT INTO sections
+            (company,
+             dates,
+             description,
+             responsabilities,
+             technologies)
+VALUES      ('Allianz Technology (Madrid)',
+             '03/2022 - today',
+'TAD Factory EU (Poland/Spain/PT BU) Support of applications: POLA.NET/Globtrotter',
+'Responsibilities:',
+'Technologies:');
+
+INSERT INTO sections
+            (company,
+             dates,
+             description,
+             responsabilities,
+             technologies)
+VALUES      ('atSistemas (Madrid)',
+             '10/2017 - 03/2022',
+'Santander Bank (SBGM) Consultant for proyects: Everest /HSF/ User Administration/Apolo',
+'Responsibilities:',
+'Technologies:');
+
+INSERT INTO sections
+            (company,
+             dates,
+             description,
+             responsabilities,
+             technologies)
+VALUES      ('Interactive Medica (Madrid)',
+             '04/2017 - 10/2017',
+'',
+'Responsibilities:',
+'Technologies:');
+-- Puedes agregar más inserciones según sea necesario
+
+-- Inserciones para la tabla RESPONSABILITIES
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (1, 'Creation of different sensors and metrics inside the monitoring tools');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (1, 'Used of CI/CD tools such as Github, Teamcity and Octopus.');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (1, 'Develop new windows services');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (1, 'Documentation of incidents and processes in Confluence.');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (1, 'Analyzing and designing the best solution to implement new requests.');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (1, 'Ensuring quality and deadlines in the resolution of incidents and requests.');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (1, 'Responsibility1');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (1, 'Responsibility1');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (1, 'Responsibility1');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (1, 'Responsibility1');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (1, 'Responsibility1');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (1, 'Responsibility1');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (1, 'Responsibility1');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (1, 'Responsibility1');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (2, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (2, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (2, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (2, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (2, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (2, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (2, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (2, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (2, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (2, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (2, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (2, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (3, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (3, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (3, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (3, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (3, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (3, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (3, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (3, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (3, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (3, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (3, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (3, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (3, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (3, 'Responsibility2');
+INSERT INTO RESPONSABILITIES (ID_SECTIONS, TEXT) VALUES (3, 'Responsibility2');
+-- Puedes agregar más inserciones según sea necesario
+
+-- Inserciones para la tabla TECHNOLOGIES
+INSERT INTO TECHNOLOGIES (ID_SECTIONS, TEXT) VALUES (1, 'Technology1');
+INSERT INTO TECHNOLOGIES (ID_SECTIONS, TEXT) VALUES (2, 'Technology2');
+-- Puedes agregar más inserciones según sea necesario
+
+-- Inserciones para la tabla SKILLS
+INSERT INTO SKILLS (DESCRIPTION, SCORE) VALUES ('Skill1', 'Score1');
+INSERT INTO SKILLS (DESCRIPTION, SCORE) VALUES ('Skill2', 'Score2');
+-- Puedes agregar más inserciones según sea necesario
